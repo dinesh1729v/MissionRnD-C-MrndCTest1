bd14d3358358240cd2487ecb3b04aff02e0c3d53
dinesh1729v <dinesh1729v@missionrnd.com>
2016-04-02 21:15:31 +0530

    Submit Commit

--- src/BetweenDays.cpp
+++ src/BetweenDays.cpp
@@ -27,8 +27,58 @@ struct node{
 	int data;
 	struct node *next;
 };
+int isValid(int d,int m,int y)
+{
+	int  i, month[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }, leap = 0;
+	if (y == 0)return 0;
+	if (y % 4 == 0)leap = 1;
+	if ((y % 100 == 0) && (y % 400 != 0))leap = 0;
+	if (d <= 0 || d>31)return 0;
+	if (m <= 0 || m>12)return 0;
+	if (m == 2 && d > 29)return 0;
+	if (m == 2 && leap == 1 && d <= 29)return 1;
+	if (month[m - 1] < d)return 0;
+	return 1;
+}
+int getDays(int d, int m, int y)
+{
+	int month[] = { 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }, leap = 0,days,i;
+	if (y % 4 == 0)
+	{
+		days = 366;
+		month[1] = 29;
+	}
+	if (y % 100 == 0 && y % 400 != 0)
+	{
+		days = 365;
+		month[1] = 28;
+	}
+	for (i = 0; i < m - 1; i++)
+		days -= month[i];
+	days -= (d - 1);
+	return days;
 
-
+}
 int between_days(struct node *date1head, struct node *date2head){
-	return -1;
+	if (date1head == NULL || date2head == NULL)return -1;
+	int d1=0, d2=0, m1=0, m2=0, y1=0, y2=0,cnt=0,days1,days2;
+	
+	while (date1head != NULL)
+	{
+		if (cnt >= 0 && cnt <= 1)d1 = d1 * 10 + date1head->data;
+		else if (cnt >= 2 && cnt <= 3)m1 = m1 * 10 + date1head->data;
+		else if (cnt >= 4 && cnt <= 7)y1 = y1 * 10 + date1head->data;
+	}
+	cnt = 0;
+	while (date2head != NULL)
+	{
+		if (cnt >= 0 && cnt <= 1)d2 = d2 * 10 + date2head->data;
+		else if (cnt >= 2 && cnt <= 3)m2 = m2 * 10 + date2head->data;
+		else if (cnt >= 4 && cnt <= 7)y2 = y2 * 10 + date2head->data;
+	}
+	if (isValid(d1, m1, y1) && isValid(d2, m2, y2))return -1;
+	days1 = getDays(d1, m1, y1);
+	days2 = getDays(d2, m2, y2);
+	if ((days2 - days1) < 0)return days1 - days2;
+	return days2 - days1;
 }
